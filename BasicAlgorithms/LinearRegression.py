091a3d342fe57f9b572ffeabf96f38a221e3b80a
Tony Frietto <tfrietto@gmail.com>
2020-12-14 17:25:39 -0500

    added while loop to use a test with above 90% accuracy

--- BasicAlgorithms/LinearRegression.py
+++ BasicAlgorithms/LinearRegression.py
@@ -19,27 +19,33 @@ X = numpy.array(Data.drop([Predict], 1))
 # declare and assign array Y to include only data in "Predict" column
 Y = numpy.array(Data[Predict])
 
-# takes our data from X and Y and splits into 4 groups
-# x_train is values from array X used to create line of best fit
-#       comprised of 90% of data
-# x_test is used to test our line of best fit
-#       comprised of 10% of data
-# same is true of y_train and y_test
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.1)
+# while loop to get more accurate model
+best = 0
+while best < 0.95:
+    # takes our data from X and Y and splits into 4 groups
+    # x_train is values from array X used to create line of best fit
+    #       comprised of 90% of data
+    # x_test is used to test our line of best fit
+    #       comprised of 10% of data
+    # same is true of y_train and y_test
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.1)
 
 
-# creates and teaches the model the data
-Linear = linear_model.LinearRegression()
-Linear.fit(x_train, y_train)
+    # creates and teaches the model the data
+    Linear = linear_model.LinearRegression()
+    Linear.fit(x_train, y_train)
 
-# print out accuracy as a percent
-# accuracy is different every run because the declaration of x_train, x_test, etc. chooses random values
-Accuracy = Linear.score(x_test, y_test)
-print("Accuracy", Accuracy, "\n")
+    # print out accuracy as a percent
+    # accuracy is different every run because the declaration of x_train, x_test, etc. chooses random values
+    Accuracy = Linear.score(x_test, y_test)
+    print("Accuracy: ", Accuracy)
 
-# writing pickle file to save model
-with open("studentG3model.pickle", "wb") as f:
-    pickle.dump(Linear, f)
+    # if Accuracy of current test is better than all previous tests, update model
+    if(Accuracy > best):
+        # writing pickle file to save model
+        with open("studentG3model.pickle", "wb") as f:
+            pickle.dump(Linear, f)
+        best = Accuracy
 
 # reading pickle file
 pickle_in = open("studentG3model.pickle", "rb")
