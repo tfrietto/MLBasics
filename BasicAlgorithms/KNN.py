d988c32ca0fc5f34c9a804151ec205fd8dd4ac8b
Tony Frietto <tfrietto@gmail.com>
2020-12-15 09:50:04 -0500

    changed safety values to numeric

--- BasicAlgorithms/KNN.py
+++ BasicAlgorithms/KNN.py
@@ -5,6 +5,18 @@ import pandas
 import numpy
 from sklearn import linear_model,preprocessing
 
-# opens car.data and copys data into variable Data
+# opens car.data and copies data into variable Data
 Data = pandas.read_csv("Data_files/car.data")
 print(Data.head())
+
+# convert all the values in safety to numeric values
+# low corresponds to 0, med to 1, and high to 2
+def convertSafetyToNum(x):
+    if x == "low":
+        return 0
+    elif x == "med":
+        return 1
+    else:
+        return 2
+Data["safety"] = numpy.array(list(map(convertSafetyToNum,Data["safety"])))
+
