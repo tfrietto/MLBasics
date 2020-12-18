ecdbbf2dec7693016ec4dade2ba7e9488b758d44
Tony Frietto <tfrietto@gmail.com>
2020-12-18 11:52:49 -0500

    made a predictions and accuracy test

--- BasicAlgorithms/SVM.py
+++ BasicAlgorithms/SVM.py
@@ -1,6 +1,7 @@
 import sklearn
 from sklearn import datasets
 from sklearn import svm
+from sklearn import metrics
 
 # importing a data set from datasets
 cancerData = datasets.load_breast_cancer()
@@ -9,11 +10,15 @@ cancerData = datasets.load_breast_cancer()
 X = cancerData.data
 Y = cancerData.target
 
-# will be used later to replace the 1's and 0's
-targetNames = ["Malignant", "Benign"]
-
 # assigning our training data and testing data
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.1)
 
+# create and train model
+clf = svm.SVC(kernel="linear")
+clf.fit(x_train,y_train)
+
+# create predictions and test accuracy
+predictions = clf.predict(x_test)
+acc = metrics.accuracy_score(y_test, predictions)
 
 
